28dd9f9b9b0b33e4ed07bcf5d123df3421f31b77
Abhishek Palve <140493725+abhishek172003@users.noreply.github.com>
2025-08-27 14:39:04 +0530

    Delete chatbot langgraph directory

--- chatbot langgraph/threadingcopy.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from langgraph_backend import chatbot
-from langchain_core.messages import HumanMessage
-import uuid
-
-
-st.sidebar.title("LangGraph Chatbot")
-st.sideebar.button("new chat")
